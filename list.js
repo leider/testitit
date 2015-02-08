d89fabbac1cd8062e772d6c625e08f5c40db76fa
leider <derleider@web.de>
2015-02-08 16:38:28 +0100

    new command

---
+++ list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var
+  homedir = process.argv[2],
+  domain = process.argv[3],
+  ezmlm = require('ezmlm-node')(homedir, domain, '.ezmlmrc'),
+  group = process.argv[4];
+
+ezmlm.usersOfList(group, ezmlm.defaultOptions, null);
